bc665cfce364dd508c2a99fa87e95b24e3757080
Lars Mühmel <larsmuehmel@web.de>
2021-01-01 21:27:55 +0100

    disable auto-login to fix gnome startup

--- configuration.nix
+++ configuration.nix
@@ -159,13 +159,8 @@
   services.xserver.libinput.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager = {
-    gdm.enable = true;
-    autoLogin = {
-      enable = true;
-      user = "lars";
-    };
-  };
+  services.xserver.displayManager.gdm.enable = true;
+
   services.xserver.desktopManager.gnome3.enable = true;
   services.gnome3 = { chrome-gnome-shell.enable = true; };
 
