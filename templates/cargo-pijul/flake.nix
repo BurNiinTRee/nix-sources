59edd0949108120ed4ad75bcedd3eccf35361fa4
Lars Mühmel <larsmuehmel@web.de>
2021-03-05 18:17:08 +0100

    move template to default nixpkgs rust, not mozilla-nixpkgs overlay

--- templates/cargo-pijul/flake.nix
+++ templates/cargo-pijul/flake.nix
@@ -5,21 +5,11 @@
     flake-utils.url = "github:numtide/flake-utils";
     import-cargo.url = "github:edolstra/import-cargo";
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    mozilla = {
-      url = "github:mozilla/nixpkgs-mozilla";
-      flake = false;
-    };
   };
 
   outputs = { self, flake-utils, import-cargo, nixpkgs, mozilla }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      mozpkgs = pkgs.callPackage (mozilla + "/package-set.nix") { };
-      rust = (mozpkgs.rustChannelOf {
-        date = "2021-01-05";
-        channel = "nightly";
-        sha256 = "sha256-dHTGzRdl6w+BRSLKUZkh5U9tizFZRrqsX0rF3ChQvK4=";
-      }).rust;
       cargo-vendor = (import-cargo.builders.importCargo {
         lockFile = ./Cargo.lock;
         inherit pkgs;
@@ -30,7 +20,8 @@
         src = self;
         nativeBuildInputs = [
           cargo-vendor.cargoHome
-          rust
+          pkgs.rustc
+          pkgs.cargo
         ];
         buildPhase = ''
           cargo build --offline ${if isRelease then "--release" else ""}
