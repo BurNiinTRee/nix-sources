4fb91f7766f88e5d5220d10b8fcc58cdbf85a5ed
Lars Mühmel <larsmuehmel@web.de>
2021-03-08 14:30:16 +0100

    use upstream nextcloud modules, since acme stuff is fixed

--- server/nextcloud-server.nix
+++ server/nextcloud-server.nix
@@ -2,16 +2,19 @@
 let domain = "cloud.muehml.eu";
 in
 {
-  imports = [ ../modules/nextcloud.nix ];
-  disabledModules = [ "services/web-apps/nextcloud.nix" ];
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud20;
+    package = pkgs.nextcloud21;
     autoUpdateApps.enable = true;
     hostName = domain;
     https = true;
     config = {
       adminpass = "password";
     };
+  };
+
+  services.nginx.virtualHosts."cloud.muehml.eu" = {
+    enableACME = true;
+    forceSSL = true;
   };
 }
