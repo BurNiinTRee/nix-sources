293fbc338c4960a58a7535e0c59fd12cf14ac1da
Lars Mühmel <larsmuehmel@web.de>
2021-03-22 17:05:44 +0100

    added Pluto.jl to sysimage

--- update-julia.jl
+++ update-julia.jl
@@ -3,7 +3,7 @@ using Pkg
 
 Pkg.update()
 
-create_sysimage([:OhMyREPL, :Plots, :CodeTracking, :Revise],
+create_sysimage([:OhMyREPL, :Plots, :CodeTracking, :Revise, :LaTeXStrings, :Pluto],
                 precompile_statements_file="./julia-stuff/repl_precompile.jl", 
-                precompile_execution_file=["./julia-stuff/plots_precompile.jl", "./julia-stuff/mkPlots.jl"],
+                precompile_execution_file=["./julia-stuff/plots_precompile.jl", "./julia-stuff/mkPlots.jl", "./julia-stuff/pluto_precompile.jl"],
                 sysimage_path="./julia-stuff/sysimage.so")
