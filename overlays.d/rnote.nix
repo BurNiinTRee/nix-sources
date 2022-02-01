e838309202f320caf7fc29ea09182ed828e298c0
Lars Mühmel <larsmuehmel@web.de>
2022-02-01 19:59:04 +0100

    add rnote package overlay

---
+++ overlays.d/rnote.nix
@@ -0,0 +1,63 @@
+self: super:
+let
+  poppler = self.poppler.overrideAttrs (o: rec {
+    version = "22.01.0";
+    src = self.fetchurl {
+      url = "https://poppler.freedesktop.org/poppler-${version}.tar.xz";
+      sha256 = "sha256-fTSTBWtbhkE+XGk8LK4CxcBs2OYY0UwsMeLIS2eyMT4=";
+    };
+  });
+  gstreamer = self.gst_all_1.gstreamer.overrideAttrs (o: rec {
+    version = "1.18.5";
+    src = self.fetchurl {
+      url = "https://gstreamer.freedesktop.org/src/${o.pname}/${o.pname}-${version}.tar.xz";
+      sha256 = "sha256-VYYiMqY0Wbv1ar694whcqa7CEbR46JHazqTW34yv6Ao=";
+    };
+  });
+in
+{
+  rnote = self.stdenv.mkDerivation rec {
+    pname = "rnote";
+    version = "0.2.5";
+
+    src = self.fetchFromGitHub {
+      repo = pname;
+      owner = "flxzt";
+      rev = "v${version}";
+      sha256 = "sha256-4C0jsKmZeqzlEzJk9XLF41CZDFVsSklRmizY7N4zz+A=";
+    };
+
+    cargoDeps = self.rustPlatform.fetchCargoTarball {
+      inherit src;
+      name = "${pname}-${version}";
+      hash = "sha256-CvxUynDRnPGoqWxxzdZoQUys/kMwj3f9/IXVA4EqiNU=";
+    };
+
+    patches = [
+      # enables us to use gtk4-update-icon-cache instead of gtk3 one
+      ../patches/rnote.patch
+    ];
+
+
+
+    nativeBuildInputs = with self; [
+      pkg-config
+      ninja
+      meson
+      rustPlatform.cargoSetupHook
+      rustPlatform.rust.cargo
+      rustPlatform.rust.rustc
+      desktop-file-utils
+      shared-mime-info
+      wrapGAppsHook
+    ];
+
+    buildInputs = with self; [
+      gtk4
+      libadwaita
+      poppler
+      gstreamer
+      libxml2
+    ];
+  };
+}
