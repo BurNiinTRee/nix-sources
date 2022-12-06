5f559842fae097a94cd585b2079728ed26090e8c
Lars Mühmel <lars@muehml.eu>
2022-12-06 10:23:36 +0100

    refactor flake

--- flake.nix
+++ flake.nix
@@ -2,47 +2,47 @@
   description = "My Nixos System";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts.url = "github:BurNiinTRee/flake-parts";
   };
 
-  outputs = { self, nixpkgs, agenix, home-manager, nixpkgs-unstable, flake-parts, ... }@inputs:
+  outputs = { self, nixpkgs, agenix, home-manager, nixpkgs-stable, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit self; } {
       systems = [ "x86_64-linux" ];
 
       perSystem = { config, pkgs, system, ... }: {
         legacyPackages = pkgs;
-        nixpkgs.path = nixpkgs-unstable;
         nixpkgs.overlays = [ agenix.overlay ];
 
         devShells.default = pkgs.mkShell {
           packages = [ pkgs.nixos-rebuild pkgs.agenix ];
           RULES = "${self}/secrets/secrets.nix";
         };
-
+        formatter = pkgs.nixpkgs-fmt;
       };
       flake = {
         homeConfigurations = {
           user = home-manager.lib.homeManagerConfiguration {
-            pkgs = import nixpkgs-unstable {
+            pkgs = import nixpkgs {
               system = "x86_64-linux";
             };
             modules = [ ./home/user.nix ];
+            extraSpecialArgs = { flakeInputs = inputs; };
           };
         };
 
 
         nixosConfigurations = {
-          "muehml" = nixpkgs.lib.nixosSystem {
+          "muehml" = nixpkgs-stable.lib.nixosSystem {
             system = "x86_64-linux";
             specialArgs = { flakeInputs = inputs; };
             modules = [
@@ -51,7 +51,7 @@
             ];
           };
 
-          rpi = nixpkgs.lib.nixosSystem {
+          rpi = nixpkgs-stable.lib.nixosSystem {
             system = "aarch64-linux";
             specialArgs = { nixpkgs = nixpkgs; };
             modules = [
