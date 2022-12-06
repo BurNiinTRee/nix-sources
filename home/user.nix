e75cd6b166af182aa9da10af47aa8943aa274b26
Lars Mühmel <lars@muehml.eu>
2022-12-06 10:26:57 +0100

    registry shenanigans

--- home/user.nix
+++ home/user.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, flakeInputs, ... }:
 {
 
   programs.home-manager.enable = true;
@@ -21,12 +21,34 @@
   };
 
 
-  nix.registry.pkgs = {
-    from = { id = "pkgs"; type = "indirect"; };
-    to = { path = "/var/home/user/nix-sources"; type = "path"; };
+  nix = {
+    registry = {
+      nixpkgs.flake = flakeInputs.nixpkgs;
+
+      pkgs.to = {
+        path = "/var/home/user/nix-sources";
+        type = "path";
+      };
+      flake-parts.to = { type = "github"; owner = "BurNiinTRee"; repo = "flake-parts"; };
+    };
+
+    package = pkgs.nixUnstable;
+
+    settings =
+      let emptyFlakeRegistry = pkgs.writeText "flake-registry.json" (builtins.toJSON { flakes = [ ]; version = 2; });
+      in {
+        extra-experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+        sandbox = true;
+        connect-timeout = 5;
+        log-lines = 25;
+        min-free = 54534824;
+        fallback = true;
+        warn-dirty = false;
+        auto-optimise-store = true;
+        max-jobs = 8;
+        flake-registry = emptyFlakeRegistry;
+      };
   };
-
-
   targets.genericLinux.enable = true;
   home = {
     username = "user";
