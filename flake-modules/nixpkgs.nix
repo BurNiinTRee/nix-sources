15d2fabf33f4160adbfd546b6d4f340e7ead685c
Lars Mühmel <lars@muehml.eu>
2023-01-06 00:23:32 +0100

    simplify module
    we removed any options related to cross compilation,
    all those uses are easily coverd by the [variants module](https://github.com/hercules-ci/hercules-ci-agent/tree/master/variants.nix)

--- flake-modules/nixpkgs.nix
+++ flake-modules/nixpkgs.nix
@@ -1,19 +1,7 @@
-# Taken almost verbatim from Idesgoui at https://github.com/hercules-ci/flake-parts/issues/35
-# and upstream flake-parts at https://github.com/hercules-ci/flake-parts/blob/main/modules/nixpkgs.nix
-{
-  flake-parts-lib,
-  config,
-  inputs,
-  ...
-}: let
-  rootConfig = config;
-  inherit (flake-parts-lib) mkPerSystemOption;
-in {
-  options.perSystem = mkPerSystemOption ({
+{...}: {
+  config.perSystem = {
     config,
     lib,
-    system,
-    moduleType,
     inputs',
     ...
   }: let
@@ -58,29 +46,6 @@ in {
           default = [];
         };
       };
-      crossSystems = mkOption {
-        type = types.attrsOf (types.oneOf [types.str types.attrs]);
-        default = builtins.filter (c: system != c && config.selectCrossSystem c) (map lib.systems.elaborate rootConfig.systems);
-
-        apply = systems: builtins.mapAttrs (_: lib.systems.elaborate) systems;
-      };
-
-      selectCrossSystem = mkOption {
-        type = types.functionTo types.bool;
-
-        default = crossSystem: let
-          local = lib.systems.elaborate system;
-        in
-          # Can only build MacOS stuff from MacOS
-          (crossSystem.isMacOS -> local.isMacOS)
-          # https://github.com/NixOS/nixpkgs/issues/137877
-          && (local.isMacOS && local.isAarch64 -> !crossSystem.isLinux);
-      };
-
-      x = mkOption {
-        type = types.lazyAttrsOf moduleType;
-        default = {};
-      };
     };
 
     config = {
@@ -97,15 +62,6 @@ in {
             crossSystem = cfg.hostPlatform;
           })
       );
-      nixpkgs.hostPlatform =
-        lib.mkIf (config ? _module.args.crossSystem)
-        config._module.args.crossSystem;
-
-      x =
-        lib.mkIf (!(config ? _module.args.crossSystem))
-        (builtins.mapAttrs
-          (_: c: {_module.args.crossSystem = c;})
-          config.crossSystems);
     };
-  });
+  };
 }
