b60b08f2bd0548ecf0dbcad3189df1ea89478341
Lars Mühmel <lars@muehml.eu>
2023-08-05 15:12:27 +0200

    change git email

--- inner/home/user/git.nix
+++ inner/home/user/git.nix
@@ -2,7 +2,7 @@
   programs.git.enable = true;
   xdg.configFile."git/config".text = ''
     [user]
-    email = "lars@muehml.eu"
+    email = "larsmuehmel@web.de"
     name = "Lars Mühmel"
     signingKey = "Lars Mühmel <larsmuehmel@web.de>"
 
