99cb21c57fda3e261a513dcb264492f0546ac764
Lars Mühmel <larsmuehmel@web.de>
2023-08-06 22:53:29 +0200

    add command_not_found hook in nushell

--- inner/home/user/nushell.nix
+++ inner/home/user/nushell.nix
@@ -17,6 +17,19 @@
             completer: $carapace_completer
           }
         }
+        hooks: {
+          command_not_found: {
+            |cmd_name| (
+              try {
+                ${pkgs.writeScript "command-not-found" ''
+                  #!${pkgs.bash}/bin/bash
+                  source ${config.programs.nix-index.package}/etc/profile.d/command-not-found.sh
+                  command_not_found_handle "$@"
+                ''} $cmd_name
+              }
+            )
+          }
+        }
       }
     '';
   };
