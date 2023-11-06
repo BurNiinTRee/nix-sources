aba4b5d8fd7ece41f446f5b23c4f1dd1dda1256e
Lars Mühmel <larsmuehmel@web.de>
2023-11-06 14:03:45 +0100

    home/user: helix: update to new language config

--- modules/home/user/helix.nix
+++ modules/home/user/helix.nix
@@ -1,31 +1,38 @@
 {...}: {
   programs.helix = {
     enable = true;
-    languages.language = [
-      {
-        name = "nix";
-        formatter.command = "alejandra";
-      }
-      {
-        name = "rust";
-        config.check.command = "clippy";
-      }
-      {
-        name = "blueprint";
-        file-types = ["blp"];
-        indent = {
-          tab-width = 2;
-          unit = " ";
+    languages = {
+      language = [
+        {
+          name = "nix";
+          formatter.command = "alejandra";
+        }
+        {
+          name = "rust";
+        }
+        {
+          name = "blueprint";
+          file-types = ["blp"];
+          indent = {
+            tab-width = 2;
+            unit = " ";
+          };
+          scope = "text.blp";
+          roots = ["meson.build"];
+          comment-token = "//";
+          language-servers = ["blueprint-compiler"];
+        }
+      ];
+      language-server = {
+        rust-analyzer = {
+          config.check.command = "clippy";
         };
-        scope = "text.blp";
-        roots = ["meson.build"];
-        comment-token = "//";
-        language-server = {
+        blueprint-compiler = {
           command = "blueprint-compiler";
           args = ["lsp"];
         };
-      }
-    ];
+      };
+    };
     settings.theme = "onelight";
   };
 
