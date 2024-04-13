2cb256ff68d04bfa49fa116bfd9043a779fae7a3
Lars Mühmel <larsmuehmel@web.de>
2024-04-13 10:41:09 +0200

    wezterm config that doesn't quite work

--- modules/home/user/wezterm.nix
+++ modules/home/user/wezterm.nix
@@ -11,6 +11,18 @@
 
       config.hide_mouse_cursor_when_typing = false
 
+      local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
+      if success then
+        config.xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+      end
+
+      local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
+      if success then
+        config.xcursor_size = tonumber(stdout)
+      end
+
+      config.xcursor_theme = xcursor_theme
+      config.xcursor_size = xcursor_size
       return config
     '';
   };
