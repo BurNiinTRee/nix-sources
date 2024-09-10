5512bbe3515b1f6511a28b76ebdd358b8e1f23be
Lars Mühmel <larsmuehmel@web.de>
2024-09-10 18:49:49 +0200

    wezterm: change mouse bindings

--- modules/home/user/wezterm.nix
+++ modules/home/user/wezterm.nix
@@ -3,6 +3,7 @@
     enable = true;
     extraConfig = ''
       local config = wezterm.config_builder()
+      local act = wezterm.action
 
       config.default_prog = { 'nu' }
       config.font = wezterm.font 'Fira Code'
@@ -20,6 +21,24 @@
       if success then
         config.xcursor_size = tonumber(stdout)
       end
+
+      config.mouse_bindings = {
+        {
+          event = { Up = { streak = 1, button = 'Left' } },
+          mods = 'NONE',
+          action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+        },
+
+        -- and make CTRL-Click open hyperlinks
+        {
+          event = { Up = { streak = 1, button = 'Left' } },
+          mods = 'CTRL',
+          action = act.OpenLinkAtMouseCursor,
+        },
+        -- NOTE that binding only the 'Up' event can give unexpected behaviors.
+        -- Read more below on the gotcha of binding an 'Up' event only.
+      }
+
 
       config.xcursor_theme = xcursor_theme
       config.xcursor_size = xcursor_size
